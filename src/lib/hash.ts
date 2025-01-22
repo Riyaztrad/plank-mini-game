0b02b11f6cf2eb2ab1f6f681f563ab717d3b41d8
Riyaz Sheikh <riyaztrad@gmail.com>
2025-01-23 01:08:10 +0530

    latest code

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -1,4 +1,4 @@
-import { webcrypto } from 'crypto';
+import { createHash, createHmac, webcrypto } from 'crypto';
 
 export async function isHashValid(data: Record<string, string>) {
   if (process.env.NODE_ENV === 'development') return true;
@@ -6,33 +6,44 @@ export async function isHashValid(data: Record<string, string>) {
   const botToken = process.env.BOT_TOKEN;
   const encoder = new TextEncoder();
 
-  const checkString = Object.keys(data)
-    .filter((key) => key !== 'hash')
-    .map((key) => `${key}=${data[key]}`)
+  const sortedData = Object.keys(data)
     .sort()
+    .map((key) => `${key}=${data[key]}`)
     .join('\n');
 
-  const secretKey = await webcrypto.subtle.importKey(
-    'raw',
-    encoder.encode('WebAppData'),
-    { name: 'HMAC', hash: 'SHA-256' },
-    true,
-    ['sign']
-  );
+  // Secret key derived from BOT_TOKEN
+  const secretKey = createHash('sha256').update(botToken).digest();
 
-  const secret = await webcrypto.subtle.sign('HMAC', secretKey, encoder.encode(botToken));
+  // Generate hash
+  const hmac = createHmac('sha256', secretKey.toString());
+  const calculatedHash = hmac.update(sortedData).digest('hex');
+  // const checkString = Object.keys(data)
+  //   .filter((key) => key !== 'hash')
+  //   .map((key) => `${key}=${data[key]}`)
+  //   .sort()
+  //   .join('\n');
 
-  const signatureKey = await webcrypto.subtle.importKey(
-    'raw',
-    secret,
-    { name: 'HMAC', hash: 'SHA-256' },
-    true,
-    ['sign']
-  );
+  // const secretKey = await webcrypto.subtle.importKey(
+  //   'raw',
+  //   encoder.encode('WebAppData'),
+  //   { name: 'HMAC', hash: 'SHA-256' },
+  //   true,
+  //   ['sign']
+  // );
 
-  const signature = await webcrypto.subtle.sign('HMAC', signatureKey, encoder.encode(checkString));
+  // const secret = await webcrypto.subtle.sign('HMAC', secretKey, encoder.encode(botToken));
 
-  const hex = Buffer.from(signature).toString('hex');
+  // const signatureKey = await webcrypto.subtle.importKey(
+  //   'raw',
+  //   secret,
+  //   { name: 'HMAC', hash: 'SHA-256' },
+  //   true,
+  //   ['sign']
+  // );
 
-  return data.hash === hex;
+  // const signature = await webcrypto.subtle.sign('HMAC', signatureKey, encoder.encode(checkString));
+
+  // const hex = Buffer.from(signature).toString('hex');
+
+  return data.hash === calculatedHash;
 }
